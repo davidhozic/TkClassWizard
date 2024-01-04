48c39a9d5f08b6498dd00d7c835b15f4687a6807
David Hozic <davidhozic@gmail.com>
2024-01-04 10:50:23 +0100

    Bug fixes

--- tkclasswiz/__init__.py
+++ tkclasswiz/__init__.py
@@ -3,7 +3,7 @@ TkClassWizard - Library for graphically defining objects based on class annotati
 Works with Tkinter / TTKBootstrap.
 """
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 from .object_frame import *
 from .annotations import *
